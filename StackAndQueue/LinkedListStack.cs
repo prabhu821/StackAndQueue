29d94c50c7449bd619b19f39f3c04044b41d2764
rishiraj007 <rishiraj@mounttalent.com>
2023-06-20 00:35:42 +0530

    [Prabhu]Add: Ability to Peek And Pop from stack

--- StackAndQueue/LinkedListStack.cs
+++ StackAndQueue/LinkedListStack.cs
@@ -29,6 +29,7 @@ namespace StackAndQueue
             this.top = node;
             Console.WriteLine("{0} pushed to stack", value);
         }
+
         //DISPLAY
         public void Display()
         {
@@ -39,6 +40,27 @@ namespace StackAndQueue
                 Console.WriteLine(temp.data);
                 temp = temp.next;
             }
+        }
+
+        //PEEK AND POP OPERATION
+        public void Peek()     //will return top value but not delete it
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack", this.top.data);
+        }
+        public void Pop()      //will delete top data nd return
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            Console.WriteLine("Value Popped is {0}", this.top.data);
+            this.top = this.top.next;
         }
     }
 }
--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -9,7 +9,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nSelect the operation to perform \n1.Create Stack \n2.Displat Stack \n3.Exit"); 
+                Console.WriteLine("\nSelect the operation to perform \n1.Create Stack \n2.Displat Stack \n3.Peek \n4.Pop \n5.Exit"); 
                 int options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
@@ -20,6 +20,12 @@
                         break;
                     case 2:
                         stack.Display();
+                        break;
+                    case 3:
+                        stack.Peek();
+                        break;
+                    case 4:
+                        stack.Pop();
                         break;
                     default:
                         flag = false;
