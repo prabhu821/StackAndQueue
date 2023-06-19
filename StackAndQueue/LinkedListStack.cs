2a1541322b1fa0f68ef72d96417bf1f446630ffd
rishiraj007 <rishiraj@mounttalent.com>
2023-06-20 00:32:15 +0530

    [Prabhu]Add: Create Stack using Linked List

---
+++ StackAndQueue/LinkedListStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackAndQueue
+{
+    public class LinkedListStack
+    {
+        public Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        //CREATE STACK
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        //DISPLAY
+        public void Display()
+        {
+            Node temp = this.top;
+            Console.WriteLine("\nStack is as:");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -5,6 +5,27 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Stack And Queue !");
+            LinkedListStack stack = new LinkedListStack();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect the operation to perform \n1.Create Stack \n2.Displat Stack \n3.Exit"); 
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        break;
+                    case 2:
+                        stack.Display();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
